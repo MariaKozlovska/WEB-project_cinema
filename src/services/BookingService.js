9f49556d969ce915cc48587fc7650bd387796d80
MariaKozlovska <wow.kozlovska.maria@gmail.com>
2025-05-14 23:15:44 +0300

    Realize booking in BookingService.js

---
+++ src/services/BookingService.js
@@ -0,0 +1,14 @@
+const STORAGE_KEY = "bookings";
+export class BookingService {
+  static getBookings(movieId) {
+    const bookingsJson = localStorage.getItem(`bookings_${movieId}`);
+    return bookingsJson ? JSON.parse(bookingsJson) : [];
+  }
+
+  static saveBooking(movieId, seats) {
+    const currentBookings = this.getBookings(movieId);
+    const newBookings = [...currentBookings, ...seats];
+    localStorage.setItem(`bookings_${movieId}`, JSON.stringify(newBookings));
+    return newBookings;
+  }
+}
